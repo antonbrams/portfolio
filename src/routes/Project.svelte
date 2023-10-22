dc9ed6b39ec5066366e8947f8031e803fd245faf
antonbrams <dev@antonbrams.io>
2023-10-22 19:52:18 +0200

    new full screen layout

--- src/routes/Project.svelte
+++ src/routes/Project.svelte
@@ -4,7 +4,7 @@
 	import {protect_with_pin} from '$lib/logic/store.js'
 	import {pop, push} from 'svelte-spa-router'
 	import {fade} from 'svelte/transition'
-	// import {parallax} from '$lib/logic/helpers.js'
+	import {parallax} from '$lib/logic/helpers.js'
 
 	import Icon from 'mdi-svelte'
 	import Section from '$lib/ui/Section.svelte'
@@ -32,7 +32,7 @@
 
 <div
 	in:fade={{delay: 200, duration: 1000}}
-	class="fixed cursor-pointer z-[1] right top tablet:right-20 tablet:top-20"
+	class="fixed cursor-pointer z-[1] right bottom"
 	on:click={e => pop()}
 	on:keydown={e => {}}
 	role="button"
@@ -48,14 +48,12 @@
 </Section>
 
 {#each sections.filter(s => !s.hidden) as { title, description, file }, i}
-	<Section
-		class="grid tablet:grid-cols-[auto_auto] items-center justify-center gap-20 mobile:pb-30"
-	>
-		<div class:tablet:order-2={i % 2} class="contrast-[.85]">
+	<Section class="flex flex-col">
+		<div class="contrast-[.85]">
 			{#if ['jpg', 'png'].includes(file.split('.')[1])}
 				<a href={`/projects/${info.name}/${file}`} target="_blank">
 					<img
-						class="max-h-[90vh] max-w-full rounded-01"
+						class="max-h-[90vh] max-w-full w-full object-contain"
 						src="/projects/{info.name}/{file}"
 						alt=""
 					/>
@@ -63,7 +61,7 @@
 			{/if}
 			{#if ['mp4', 'mov'].includes(file.split('.')[1])}
 				<video
-					class="max-h-[90vh] max-w-full rounded-01"
+					class="max-h-[90vh] max-w-full w-full object-contain"
 					src="/projects/{info.name}/{file}"
 					muted
 					autoplay
@@ -72,12 +70,17 @@
 			{/if}
 		</div>
 
-		<div class="min-w-[20ch] max-w-[40ch]">
+		<div
+			class="max-w-[50ch] [text-shadow:_0_2px_4px_black] tablet:mx-20 -mt"
+			class:place-self-end={i % 2 == 0}
+			use:parallax={50}
+		>
 			<div class="text-10 header">{title}</div>
 			<br />
 			<div>{@html description}</div>
 		</div>
 	</Section>
+
 	{#if i == 0}
 		<Section
 			class="grid grid-cols-[repeat(auto-fit,minmax(310px,1fr))] gap grid-flow-dense"
