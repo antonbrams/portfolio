8152dcb166b9fd5ec56ddf4638f4f47797e1ac0e
antonbrams <dev@antonbrams.io>
2026-01-05 03:49:23 +0100

    text update

--- src/lib/logic/model.js
+++ src/lib/logic/model.js
@@ -6,7 +6,7 @@ export const about = [
 	{
 		title: 'Expertise',
 		items: [
-			`I'm <span class='badge'>I’m a Principal Innovation Architect</span>. I envision, design, and prototype solutions that tackle multiple challenges at once. I shape them into their most effective form—process, an app, AI agent, car dashboard, or consumer electronics—integrating design, software, and hardware, in the spirit of R&D labs like Apple Future Interfaces and Google ATAP.`,
+			`I'm <span class='badge'>I’m a Principal Innovation Architect</span>. From vision to prototype and optionally to production, I craft the fundamental solution that solves multiple challenges at once and shape it in its most effective form — GUI, AI wearable, industrial process, or car dashboard. It requires integrating design, software, and hardware into a cohesive system, like in Apple or Google R&D labs.`,
 		],
 	},
 	{
